bc99b34f27a85d481407f20132681cdbbb80ccdb
Juan Aquino <97471355+Jaqu382@users.noreply.github.com>
2023-01-26 23:45:54 +0000

    Populate flip card component

--- KnightsCrest/components/FlipCard.js
+++ KnightsCrest/components/FlipCard.js
@@ -1,18 +1,26 @@
+import { useEffect } from "react";
 import { View, StyleSheet, Text } from "react-native"
 import { Card } from "@rneui/base"
+import IDfront from "./IDfront";
 
 
 export default function FlipCard(props) {
-    return (
-        <View>
-            <Card styles = {styles.IDCard}>
-                <Text>
-                    Some text
-                </Text>
-            </Card>
-        </View>
-        
-        )
+    const {isFlipped, fName, lName, kCashNum, libNumber, UCFID, caste, expDate} = props;
+
+    return(
+    <View>
+        <Card>
+            <IDfront 
+            fName = {fName}
+            lName = {lName} 
+            kCashNum ={kCashNum} 
+            libNumber = {libNumber} 
+            UCFID = {UCFID}
+            caste = {caste}
+            expDate = {expDate}></IDfront>
+        </Card>
+    </View>
+    )
 }
 
 const styles = StyleSheet.create({
